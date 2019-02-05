3f55f6f97c4931629d77468bb4bf25d67963d659
abhinav85 <abhinav.rajput@upgrad.com>
2019-02-05 10:44:39 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// const request = require('request');
+const http = require('http');
+const axios = require('axios');
+
+
+var city;
+
+
+var server = http.createServer((req,resp) => {
+    city = req.url.substring(1) 
+
+    const header = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000,
+    }
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=edb908af227ecef60dc37cc42cacd0a4`
+    
+    axios({
+        method : 'get',
+        url : url,
+    }).then((res) => {
+        if(res.status === 200){
+            console.log('The first call');
+            resp.writeHead(200,header)
+            resp.end(JSON.stringify(res.data));
+        }
+    }).catch((error) => {
+        console.log('Error')
+    })
+})
+
+
+
+
+
+server.listen(3001);
